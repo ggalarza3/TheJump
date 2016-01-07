668a36ac9ad98b752ae377af9d0e7c2e58bcfb2d
gabrielgalarza <gabrgalarza@hotmail.com>
2016-01-07 17:30:36 -0500

    Add Username/Password text w/ hints Login Activity Leads to Welcome Screen

--- app/src/main/java/com/example/gabrielgalarza/thejump/MainActivity.java
+++ app/src/main/java/com/example/gabrielgalarza/thejump/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gabrielgalarza.thejump;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
     @Override
@@ -57,5 +65,16 @@ public class MainActivity extends AppCompatActivity {
         int ii = 2 + 2;
 
         int iii = i + ii;
+    }
+
+    private void loginButtonClick() {
+        startActivity(new Intent("thejump.WelcomeScreen"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.loginButton) {
+            loginButtonClick();
+        }
     }
 }
